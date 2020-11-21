702191f8bcf61155ac685f70f183eee30b3b277b
VibhaThakur1210 <vibhathakur39@gmail.com>
2020-11-20 22:05:37 -0800

    News app in java

---
+++ app/src/main/java/com/example/newsapp/Spalsh.java
@@ -0,0 +1,43 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.newsapp.parameters.MainActivity;
+
+public class Spalsh extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT = 7000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+        setContentView(R.layout.activity_spalsh);
+        //this will bind your MainActivity.class file with activity_main.
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Spalsh.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
